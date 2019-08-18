4a27e0d2d57284accf325ba8d6f84c18a07e453c
luisdaRC <luixdani123@gmail.com>
2019-08-18 01:45:54 -0500

    Componentes creados (7 páginas: Login, Register, navbar, MainUser, Main Admin, PageNotFound)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './componentes/home-page/home-page.component';
+import { RegisterPageComponent } from './componentes/register-page/register-page.component';
+import { LoginPageComponent } from './componentes/login-page/login-page.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { MainAdminComponent } from './componentes/main-admin/main-admin.component';
+import { MainUserComponent } from './componentes/main-user/main-user.component';
+import { NotFoundPageComponent } from './componentes/not-found-page/not-found-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    RegisterPageComponent,
+    LoginPageComponent,
+    NavbarComponent,
+    MainAdminComponent,
+    MainUserComponent,
+    NotFoundPageComponent
   ],
   imports: [
     BrowserModule,
